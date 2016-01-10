f5fadc807654e2c3a463ca50aed26ed640e18333
IndianTrinity <viki2010nkt@gmail.com>
2016-01-10 13:57:26 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Organ Donation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Vikas Yadav, Gajanand Bohara, Rohitash Yadav">
+    <meta name="description" content="Show your support for Organ Donation">
+
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <link rel="stylesheet" href="//yui.yahooapis.com/pure/0.6.0/pure-min.css">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Merriweather:400,400italic,700">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <h1>Organ Donation</h1>
+    <div class="container">
+      <p>I support Organ DOnation.</p>
+      <p><em>Upload your profile picture to generate one like the shown below. Change your profile picture to show your support for net neutrality and make people aware to not to respond to those notifications about mail to TRAI.</em></p>
+      <div class="transform-app">
+        <input type="file" id="fileElem" class="hidden">
+        <button class="pure-button pure-button-primary" id="select-photo">Select profile photo</button><br>
+        <canvas id="canvas" class="magic pure-img" width="800" height="800"></canvas><br>
+        <a href="#" id="download-btn" download="profile-pic.png"><button class="pure-button download-btn">Download</button></a>
+      </div>
+      <div class="push"></div>
+    </div>
+    <footer class="footer">
+      <div class="container">
+        Made with hate by <a href="http://www.facebook.com/viki2010nkt">Vikas Yadav</a>.
+      </div>
+    </footer>
+    <script src="script.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-71762856-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+  </body>
+</html>
+Status API Training Shop Blog About Pricing
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
